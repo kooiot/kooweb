d332867010db52a9deb36cd877cc9e4562f32a66
Dirk Chang <dirk@kooiot.com>
2014-06-27 12:23:24 +0800

    do not encode it again

--- apps/store/controller/tpl/download.lua
+++ apps/store/controller/tpl/download.lua
@@ -1,10 +1,20 @@
 return {
 	get = function(req, res)
 		local cjson = require 'cjson'
-		local appname = req:get_arg('appname')
-		local name = req:get_arg('name')
-		local content = {appname=appname, name=name}
-		res.headers['Content-Type'] = 'application/json; charset=utf-8'
-		res:write(cjson.encode(content))
+		local app_path = req:get_arg('app_path')
+		local path = req:get_arg('path')
+
+		local db = app.model:get('db')
+		db:init()
+
+		local t, err = db:get_tpl(app_path, path)
+		local content = t.content
+
+		if content then
+			res.headers['Content-Type'] = 'application/json; charset=utf-8'
+			res:write(content)
+		else
+			lwf.exit(403, err)
+		end
 	end
 }
