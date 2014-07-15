3b8aff49b1f4dc816498824c8d2fa4f9c59a0aee
Dirk Chang <dirk@kooiot.com>
2014-07-15 08:54:07 +0800

    show 10240 datapoints

--- apps/cloud/controller/device/data.lua
+++ apps/cloud/controller/device/data.lua
@@ -14,7 +14,7 @@ return {
 		local data = app.model:get('data')
 		data:init()
 
-		local list = data:list(key, path)
+		local list = data:list(key, path, 10240)
 
 		--[[
 		res.headers['Content-Type'] = 'application/json; charset=utf-8'
